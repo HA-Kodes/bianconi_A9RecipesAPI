3532c0bfd5c9cd15b9c4d431002efd99c7037c6e
HA-Kodes <2BianconiHa@gmail.com>
2025-02-01 21:06:33 -0800

    Added Application

---
+++ src/main/java/com/coderscampus/A9RecipesApplication.java
@@ -0,0 +1,11 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A9RecipesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(A9RecipesApplication.class, args);
+    }
+}
